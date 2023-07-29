afde0f877288511212a690163d49a9c7cce669f9
Zwina <35114051+Zwina@users.noreply.github.com>
2023-07-29 11:57:07 +0200

    try router2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'livres/index.html'),
-      },
-    },
-  },
+  
 })
