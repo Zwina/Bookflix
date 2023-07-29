eebba11c66ed078242fcae9d4c602e123667517d
Zwina <35114051+Zwina@users.noreply.github.com>
2023-07-29 12:40:53 +0200

    css good

--- src/Components/Header/Banner.jsx
+++ src/Components/Header/Banner.jsx
@@ -1,21 +1,12 @@
-import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import "./banner.css";
 
 function Banner() {
   return (
-    <>
-      <div className="banner">
-        <Container>
         <Image
                 src="/image/banner.jpg"
-                style={{ backgroundSize: "cover", height: "200px", objectFit: "cover", objectPosition: 'center'}}
-                fluid
-                text="Second slide"
+                style={{ backgroundSize: "cover", width:"100%", height: "200px", objectFit: "cover", objectPosition: 'center'}}
               />
-        </Container>
-      </div>
-    </>
   );
 }
 
